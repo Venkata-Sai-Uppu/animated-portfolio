4276daca7ecd5ddc2a3b7ddadb7393cfae9b1e34
Venkata Sai Uppu <venkatasaiuppu9@gmail.com>
2025-04-14 16:04:26 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Portfolio from './src/animated-portfolio'
+import Portfolio from './animated-portfolio'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
